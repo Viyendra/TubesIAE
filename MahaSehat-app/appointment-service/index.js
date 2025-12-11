71ab35ddd402ceddde77b0761a14ffe863de5600
Viyendra <muhammadviyendra@gmail.com>
2025-12-12 01:08:31 +0700

    Booked

--- MahaSehat-app/appointment-service/index.js
+++ MahaSehat-app/appointment-service/index.js
@@ -1,19 +1,48 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3002;
-let appointments = [];
+let appointments = []; 
+
+setInterval(() => {
+    const now = new Date();
+    console.log(`[SYSTEM] Checking schedules at ${now.toLocaleTimeString()}...`);
+
+    appointments.forEach(appt => {
+        const scheduleDate = new Date(`${appt.date}T${appt.time}`);
+        
+        if (appt.status === 'booked' && scheduleDate < now) {
+            appt.status = 'completed';
+            console.log(`Jadwal ID ${appt.id} selesai automatically.`);
+        }
+    });
+}, 60000);
 
 app.post('/create', (req, res) => {
     const { doctorName, date, time } = req.body;
-    const newSlot = { id: appointments.length + 1, doctorName, date, time, status: 'available' };
+    const newSlot = { 
+        id: appointments.length + 1, 
+        doctorName, 
+        date, 
+        time, 
+        status: 'available',
+        patientName: null 
+    };
     appointments.push(newSlot);
-    res.json(newSlot);
+    res.json({ message: "Jadwal dibuat", data: newSlot });
 });
 
 app.get('/', (req, res) => {
     res.json(appointments);
+});
+
+app.get('/doctor/:doctorName', (req, res) => {
+    const { doctorName } = req.params;
+    const docSchedules = appointments.filter(a => a.doctorName === doctorName);
+    res.json(docSchedules);
 });
 
 app.post('/book', (req, res) => {
